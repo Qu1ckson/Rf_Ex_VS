d766582b480a9b8a46fea5d27eba790924cb48e3
Qu1ckson <quick625@yandex.ru>
2016-10-23 17:22:30 +0300

    first commit
    first

---
+++ rf_ex_vs/rf_ex_vs/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Qu1ckson.rf_ex_vs
+{
+    static class GuidList
+    {
+        public const string guidrf_ex_vsPkgString = "42b06706-80c0-4fd0-9c07-e2b6ab2708ea";
+        public const string guidrf_ex_vsCmdSetString = "a6d3fa06-21e9-4a92-b444-9233360581c4";
+        public const string guidToolWindowPersistanceString = "b7cd1bd3-8f20-4a6f-b3fa-13188d06e5e4";
+
+        public static readonly Guid guidrf_ex_vsCmdSet = new Guid(guidrf_ex_vsCmdSetString);
+    };
+}
